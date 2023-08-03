ec639c3df417108fb63e7bb689a4e3762c68cac6
mchung03 <minhee.chung14@gmail.com>
2023-08-03 00:54:39 -0700

    all tables created sql

--- db/schemas.sql
+++ db/schemas.sql
@@ -3,17 +3,31 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE departments (
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
     role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    job_title VARCHAR(30) NOT NULL,
-    department_name VARCHAR(30) NOT NULL,
-    salary INT NOT NULL
-    FOREIGN KEY (department_name)
-    REFERENCES departments(name)
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+    FOREIGN KEY (id)
+    REFERENCES employee(manager_id)
     ON DELETE SET NULL
 );
