190151ef05b7450b5d5240e9694a84d65917130f
mchung03 <minhee.chung14@gmail.com>
2023-08-03 20:38:25 -0700

    departments

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const table = require('console.table')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,27 +9,60 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
-inquirer
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'employee_db'
+    },
+    console.log(`Connected to employee_db`)
+)
+db.connect(function(err) {
+    if(err) {
+        console.log(err)
+    }
+})
+
+function showPrompt() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What would you like to do?',
+                name: 'action',
+                choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+            }
+        ])
+        .then(ans => {
+            if(ans.action == 'view all departments') {
+                viewDepartments()
+            } else if(ans.action == 'add a department') {
+                addDepartments()
+            }
+        })
+}
+function viewDepartments() {
+    db.query('SELECT * FROM department', function(err, data) {
+        console.table(data)
+        showPrompt()
+    })
+}
+function addDepartments() {
+    inquirer
     .prompt([
         {
-            type: 'list',
-            message: 'What would you like to do?',
-            name: 'action',
-            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+            type: 'input',
+            message: 'What is the name of the department?',
+            name: 'department_name'
         }
     ])
     .then(ans => {
-        if(ans.action == 'view all departments') {
-            
-        }
+        db.query(`INSERT INTO department(name) VALUES (?)`, [ans.department_name], function(err, data){
+            console.log('Department added!')
+            showPrompt()
+        })
     })
+}
 
-// const db = mysql.createConnection(
-//     {
-//         host: 'localhost',
-//         user: 'root',
-//         password: 'password',
-//         database: 'employee_db'
-//     },
-//     console.log(`Connected to employee_db`)
-// )
+showPrompt()
